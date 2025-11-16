1122989664b9225e23ae7285e406c0d4feb7d88d
kushal-chandar <kushalchandariit@gmail.com>
2025-11-16 21:05:07 +0530

    Updates

---
+++ assets/index-CHzKkvoK.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cb0Vd1Tx.js","assets/index-G0WB43NL.js","assets/index-C02rvTzk.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-G0WB43NL.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Cb0Vd1Tx.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ assets/web-Cb0Vd1Tx.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-G0WB43NL.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
